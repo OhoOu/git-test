ff18d041660996d529956f278092e4f37fbd307b
OHO <2664585853@qq.com>
2022-07-29 17:37:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author OHO
+ * @create 2022-07-29 17:32
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
